985bbfb056901ddb679431348a30cecdf942d099
Jarad Niemi <niemi@iastate.edu>
2014-03-07 12:58:29 -0600

    made path relative

--- scripts/evalLogLoss.R
+++ scripts/evalLogLoss.R
@@ -4,10 +4,13 @@
 
 source("logLoss.R")
 
-tourneyRes = read.csv("/Users/Nick/Documents/MarchMadness/tourney_results.csv", stringsAsFactors = FALSE)
-testSub1 = read.csv("/Users/Nick/Documents/MarchMadness/submit_0127LS.csv", stringsAsFactors = FALSE)
-testSub2 = read.csv("/Users/Nick/Documents/MarchMadness/elo_chess_nohomecourt.csv", stringsAsFactors = FALSE)
-testSub3 = read.csv("/Users/Nick/Documents/MarchMadness/Ken Pom Win Probabilities.csv", stringsAsFactors = FALSE)
+# Path now relative to scripts directory (where this file is located)
+tourneyRes = read.csv("../data/raw/tourney_results.csv", stringsAsFactors = FALSE)
+
+testSub1 = read.csv("../data/raw/submit_0127LS.csv", stringsAsFactors = FALSE)
+testSub2 = read.csv("../data/raw/elo_chess_nohomecourt.csv", stringsAsFactors = FALSE)
+testSub3 = read.csv("../data/raw/Ken Pom Win Probabilities.csv", stringsAsFactors = FALSE)
+
 logLoss(testSub1, tourneyRes)
 logLoss(testSub2, tourneyRes)
 logLoss(testSub3, tourneyRes)
