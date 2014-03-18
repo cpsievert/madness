3d003d86cc39869ce24430cc1771238aaf64ccb0
Jarad Niemi <niemi@iastate.edu>
2014-03-18 08:59:33 -0500

    renamed columns to team and opponent

--- scripts/create_opponents.R
+++ scripts/create_opponents.R
@@ -6,12 +6,12 @@
 
 regular_season = read.csv("../data/raw/regular_season_results.csv")
 
-tmp = expand.grid(teams  = sort(unique(c(regular_season$wteam, regular_season$lteam))),
+tmp = expand.grid(team  = sort(unique(c(regular_season$wteam, regular_season$lteam))),
                   season = sort(unique(as.character(regular_season$season))))
 
 games = ddply(tmp, .(season,teams), function(x) {
   this_season = regular_season[regular_season$season==x$season,]
-  data.frame(opponents = c(this_season$lteam[this_season$wteam == x$teams],
+  data.frame(opponent = c(this_season$lteam[this_season$wteam == x$teams],
                            this_season$wteam[this_season$lteam == x$teams]))
 })
 
