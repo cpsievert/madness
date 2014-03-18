3f6ee9638b03150b18454d9f21b6ba8677f1c301
Jarad Niemi <niemi@iastate.edu>
2014-03-18 09:56:52 -0500

    actual elo_chess submission

--- scripts/sagarin/matchup_statistics_write.R
+++ scripts/sagarin/matchup_statistics_write.R
@@ -7,3 +7,9 @@ write.csv(data.frame(id = paste(probs$season,"_",probs$lowerid,"_",probs$upperid
                      pred = probs$pred),
           paste("../../data/submission/",model,".csv",sep=""),
           row.names=FALSE)
+
+# write competition submission file
+tmp = read.csv(paste("../../data/submission/",model,".csv",sep=""))
+write.csv(tmp[grep("S",tmp$id),],
+          paste("../../data/submission/",model,"2014.csv",sep=""),
+          row.names=FALSE)
